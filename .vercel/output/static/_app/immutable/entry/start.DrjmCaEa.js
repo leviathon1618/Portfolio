14e8658c617ce09b2f5453179891e77be69aff07
leviathon1432 <51034215+leviathon1618@users.noreply.github.com>
2025-01-11 22:18:32 +1300

    #6

--- .vercel/output/static/_app/immutable/entry/start.DrjmCaEa.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Cl6a6SWH.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		files: {
-			appTemplate: 'static/index.html'
+			appTemplate: 'src/index.html'
 			// ... other file paths
 		}
 	}

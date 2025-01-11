c4603dfed34947c3b85d754e69c7f60b0dbc937d
leviathon1432 <51034215+leviathon1618@users.noreply.github.com>
2025-01-11 21:27:06 +1300

    trying again

---
+++ .svelte-kit/output/client/_app/immutable/entry/start.Bx-ItGeU.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B9D8i4wQ.js";export{t as start};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DyXhxXzX.js","_app/immutable/chunks/disclose-version.PQL5KBpB.js","_app/immutable/chunks/runtime.D03TYric.js","_app/immutable/chunks/legacy.CAiow5O5.js","_app/immutable/chunks/render.Dd1mBPDN.js","_app/immutable/chunks/lifecycle.2LqXZwNh.js","_app/immutable/chunks/entry.jAUwrqr8.js","_app/immutable/chunks/index-client.DhEXzatg.js"];
+export const imports = ["_app/immutable/nodes/1.C7qv87rV.js","_app/immutable/chunks/disclose-version.PQL5KBpB.js","_app/immutable/chunks/runtime.D03TYric.js","_app/immutable/chunks/legacy.CAiow5O5.js","_app/immutable/chunks/render.Dd1mBPDN.js","_app/immutable/chunks/lifecycle.2LqXZwNh.js","_app/immutable/chunks/entry.B9D8i4wQ.js","_app/immutable/chunks/index-client.DhEXzatg.js"];
 export const stylesheets = [];
 export const fonts = [];

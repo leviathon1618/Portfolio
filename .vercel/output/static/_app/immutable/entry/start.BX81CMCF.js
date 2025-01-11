9665e80f91fb02a5dff2d9ac81475add45877fb7
leviathon1432 <51034215+leviathon1618@users.noreply.github.com>
2025-01-11 22:42:15 +1300

    #8

--- .vercel/output/static/_app/immutable/entry/start.BX81CMCF.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.X6hUnfDe.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+//import index_url from './src/index.html';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,11 +12,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		files: {
-			appTemplate: './src/index.html'
-			// ... other file paths
-		}
+		adapter: adapter()
+		// files: {
+		// 	appTemplate: index_url
+		// 	// ... other file paths
+		// }
 	}
 };
 
